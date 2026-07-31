9f1998631633af99ea92ea97ed83fd621e9497ca
Sloft20 <thiagoelson10@gmail.com>
2026-07-31 12:20:18 -0300

    Atualização do banco

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://eyzsylmxlrouxxpxhtlr.supabase.co';
+const supabaseUrl = 'https://ltaznustoryrpotxzrdj.supabase.co';
 // Substituímos pela chave 'anon' pública (Legacy JWT)
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV5enN5bG14bHJvdXh4cHhodGxyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI2OTM1ODEsImV4cCI6MjA5ODI2OTU4MX0.vDPomKPufQQ6f9YlwOwNsnN9r2yMZ9ai64L8VPBLl_4';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx0YXpudXN0b3J5cnBvdHh6cmRqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU1MDMzMDUsImV4cCI6MjEwMTA3OTMwNX0.AeTrBnwvOAf2QcSUjcE3UyK93VMkfWSaOsDH_wf3Hh8';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
