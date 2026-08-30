69ddb70a423bea6601b651f021bc615d455c09ab
Sloft20 <thiagoelson10@gmail.com>
2026-08-30 10:36:33 -0300

    chore: blindagem das chaves de API

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://ltaznustoryrpotxzrdj.supabase.co';
-// Substituímos pela chave 'anon' pública (Legacy JWT)
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx0YXpudXN0b3J5cnBvdHh6cmRqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU1MDMzMDUsImV4cCI6MjEwMTA3OTMwNX0.AeTrBnwvOAf2QcSUjcE3UyK93VMkfWSaOsDH_wf3Hh8';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
